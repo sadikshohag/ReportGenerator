bb71e26308362b51b7f16da7dbb31c1b2be0f39b
sadikshohag <sadik.ahmed.cse@ulab.edu.bd>
2018-04-18 11:21:22 +0600

    BTS report and Summary

---
+++ bts.php
@@ -0,0 +1,91 @@
+<?php
+$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "qubee";
+    $con = mysqli_connect($servername, $username, $password, $dbname);
+    $hard_details_data = array();
+    $query = "SELECT * from onair";
+    $result = mysqli_query($con,$query);
+    
+    while($row = mysqli_fetch_assoc($result))
+    {
+      $hard_details_data[]=$row;
+    }
+
+?>
+
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css"  href="style.css">
+<!------ Include the above in your HEAD tag ---------->
+
+<?php include 'menu2.php'; ?>
+<br>
+		<div class="container2">
+  
+	<div class="collapse navbar-collapse js-navbar-collapse">
+		<ul class="list-inline text-center">
+			<li><a href="onAirSite.php"><strong>On Air Site Tracker</strong></a></li>			
+			<li><a href="siteSum.php"><strong>Site list summary</strong> </a></li>					
+		</ul>
+        
+	</div><!-- /.nav-collapse -->
+ 
+</div>
+       <div class="wrapper">
+      <div class="page-header text-center">
+        <h1>QUBEE BTS Report</h1>
+      </div>
+      <div class="container">
+        <div class="table-responsive">
+        <table id="mytable" class="table table-bordered table-hover">
+            <thead>
+                <tr class="alert-success">
+                  <th>SL</th>
+                  <th>Qubee ID</th>
+                  <th>Operator</th>
+                  <th>Region</th>
+                  <th>Location</th>
+                  <th>Status</th>
+                  <th>On Air Date</th>
+                  <th>Final Soft Copy (SSD)</th>
+                  <th>Civil Vendor</th>
+                </tr>
+            </thead>
+            <tbody>
+              <?php
+                if (!empty($hard_details_data)) :
+                		$serial=0;
+                  foreach ($hard_details_data as $Oneuser) :
+                  $serial++;  
+                    ?>
+                  <tr>
+                  	<td><?php echo $serial;?></td>
+                    <td><?php echo $Oneuser['Qubee_ID'];?></td>
+                    <td><?php echo $Oneuser['Operator'];?></td>
+                    <td><?php echo $Oneuser['Region'];?></td>
+                    <td><?php echo $Oneuser['Location'];?></td>
+                    <td width="10px"><?php echo $Oneuser['Status']; ?></td>
+                    <td><?php echo $Oneuser['On_Air_Date']; ?></td>
+                    <td><?php echo $Oneuser['Final_Soft_Copy_SSD']; ?></td>
+                    <td><?php echo $Oneuser['Civil_Vendor']; ?></td>
+                  </tr>
+                    <?php
+                   
+                  endforeach;
+                endif;
+              ?>
+            </tbody>
+        </table>
+        </div>
+<!--         <div class="pagination-container">
+<nav>
+<ul class="pagination"></ul>
+</nav>
+</div> -->
+    </div>
+    </div>
