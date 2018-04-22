3f2912732facd8a593c13829321021592a126222
sadikshohag <sadik.ahmed.cse@ulab.edu.bd>
2018-04-22 16:40:02 +0600

    Misc Insert and Details

---
+++ miscInfo.php
@@ -0,0 +1,90 @@
+<?php 
+$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "qubee";
+    $con = mysqli_connect($servername, $username, $password, $dbname);
+    $hard_details_data = array();
+    $query = "SELECT `id`, `package`,`peak_bw`, `avg_bw`, `95%Down`, `available_bw` FROM `misc`";
+    $result = mysqli_query($con,$query);
+    
+    while($row = mysqli_fetch_assoc($result))
+    {
+      $hard_details_data[]=$row;
+    }
+
+
+
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Top user Dhaka</title>
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css"  href="style.css">
+</head>
+<body>
+<?php include 'menu2.php'; ?>
+
+<br>
+	<div class="container2">
+  
+ 
+  <div class="collapse navbar-collapse ">
+    <ul class="list-inline text-center">
+      <li><a href="miscInsert.php"><strong>Misc Insert</strong></a></li>     
+      <li><a href="miscInfo.php"><strong>MISC Info</strong> </a></li>  
+    </ul>
+        
+  </div><!-- /.nav-collapse -->
+ 
+</div>
+<br>
+	<div class="wrapper">
+      <div class="page-header text-center">
+        <h1>MISC Info</h1>
+      </div>
+      <div class="container">
+        <div class="table-responsive">
+        <table id="mytable" class="table table-bordered table-hover">
+            <thead>
+                <tr class="alert-success">
+                  <th>Package</th>
+                  <!-- <th>Date</th> -->
+                  <th>PEAK BW ( Down)[Mbps]</th>
+                  <th>AVG BW ( Down)[Mbps]</th>
+                  <th>95 % Down[Mbps]</th>
+                  <th>Available BW</th>
+                </tr>
+            </thead>
+            <tbody>
+              <?php
+                if (!empty($hard_details_data)) :
+                  foreach ($hard_details_data as $Oneuser) :
+                    ?>
+                  <tr>
+                    <td><?php echo $Oneuser['package'];?></td>
+                    <!-- <td><?php echo $Oneuser['date'];?></td> -->
+                    <td><?php echo $Oneuser['peak_bw'];?></td>
+                    <td><?php echo $Oneuser['avg_bw'];?></td>
+                    <td><?php echo $Oneuser['95%Down'];?></td>
+                    <td><?php echo $Oneuser['available_bw'];?></td>
+                  </tr>
+                    <?php
+                   
+                  endforeach;
+                endif;
+              ?>
+            </tbody>
+        </table>
+        </div>
+    </div>
+   </div>
+
+	
+</body>
+</html>
---
+++ miscInsert.php
@@ -0,0 +1,80 @@
+<?php 
+
+$con = mysqli_connect('localhost','root','','qubee') or die("Error in DB connection");
+if (isset($_POST['submit'])) {
+	$package=$_POST['package'];
+	$peak_bw=$_POST['peak_bw'];
+	$avg_bw=$_POST['avg_bw'];
+	$down = $_POST['95%down'];
+	$available_bw=$_POST['available_bw'];
+	$query="INSERT into misc VALUES('','$package','$peak_bw','$avg_bw','$down','$available_bw')";
+	$result = mysqli_query($con,$query);
+	mysqli_close($con);
+}
+
+
+
+ ?>
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="loginStyle.css">
+
+    <title>Admin page</title>
+  </head>
+  <body>
+    <?php include 'menu2.php'; ?>
+        <div class="container2">
+  
+  <div class="collapse navbar-collapse ">
+    <ul class="list-inline text-center">
+      <li><a href="miscInsert.php"><strong>Misc Insert</strong></a></li>     
+      <li><a href="miscInfo.php"><strong>MISC Info</strong> </a></li>  
+    </ul>
+        
+  </div><!-- /.nav-collapse -->
+ 
+</div>
+    <div class="wrapper">
+      <div class="form">
+    <form class="form-signin" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">       
+      <h2 class="form-signin-heading">MISC Info</h2>
+   <label for="package"><strong>Department: </strong></label>
+    <select name="package"  class="form-control">
+    <option selected="selected">Select Department</option>
+    <option value="DMBWU">Dhaka Mango BW Utilization</option>
+    <option value="DFBWU">Dhaka F@H BW Utilization</option>
+    <option value="DECBWU">Dhaka Earth-Communication BW Utilization</option>
+    <option value="DMBWU">Dhaka Mango BW Utilization</option>
+    <option value="DFBWU">Dhaka F@H BW Utilization</option>
+  </select><br>
+      <input type="text" class="form-control" name="peak_bw" placeholder="PEAK BW ( Down)[Mbps]" required="" autofocus="" /><br>
+      <input type="text" class="form-control" name="avg_bw" placeholder="AVG BW ( Down)[Mbps]" required=""/><br>
+      <input type="text" class="form-control" name="95%down" placeholder="95 % Down[Mbps]" required=""/><br>
+      <input type="text" class="form-control" name="available_bw" placeholder="Available BW" required=""/><br>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Submit</button>   
+    </form>
+  </div>
+</div>
+
+  <footer>
+      <h4>&copy; Copyright 2018, QUBEE</h4>
+    </footer>
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+  </body>
+</html>
